e004377044808a7f8999761288d31f394a1a5159
sosimei <101724014+sosimei@users.noreply.github.com>
2025-05-09 00:05:51 +0900

    Delete netlify/functions/api.js

--- netlify/functions/api.js
+++
@@ -1,8 +0,0 @@
-// 이 파일은 더 이상 사용하지 않습니다.
-// 대신 각 API 경로에 해당하는 별도 파일을 사용합니다:
-// - clusters-hot.js: /clusters/hot
-// - clusters-latest.js: /clusters/latest
-// - clusters-hot-category.js: /clusters/hot/{category}
-// - clusters-latest-category.js: /clusters/latest/{category}
-// - clusters-hot-id.js: /clusters/hot/{id}
-// - clusters-latest-id.js: /clusters/latest/{id}

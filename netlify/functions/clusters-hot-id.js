56134e53fad7e0577aede596f28c60d8712b8e4a
sosimei <101724014+sosimei@users.noreply.github.com>
2025-05-09 12:08:51 +0900

    Fix hot news detail API to support both path and query parameters

--- netlify/functions/clusters-hot-id.js
+++ netlify/functions/clusters-hot-id.js
@@ -7,16 +7,26 @@ exports.handler = async function(event, context) {
       return respond(200, {});
     }
 
+    console.log('clusters-hot-id.js 호출됨:', event.path, event.httpMethod);
+
     // 경로에서 ID 파라미터 추출
     const pathParts = event.path.split('/');
     const id = pathParts[pathParts.length - 1];
 
-    if (!id) {
+    // 쿼리 파라미터로도 ID를 받을 수 있도록 함
+    const queryId = event.queryStringParameters?.id;
+    
+    // 경로 파라미터 또는 쿼리 파라미터 중 하나로 ID를 설정
+    const clusterId = id || queryId;
+
+    console.log('추출된 클러스터 ID:', clusterId);
+
+    if (!clusterId) {
       return respond(400, formatErrorResponse('ID parameter is required', 'ERROR400'));
     }
 
     // 유효한 ObjectId 형식인지 확인
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+    if (!clusterId.match(/^[0-9a-fA-F]{24}$/)) {
       return respond(400, formatErrorResponse('Invalid ID format', 'ERROR400'));
     }
 
@@ -24,7 +34,7 @@ exports.handler = async function(event, context) {
     const collection = await connectToDB();
 
     // 세부 핫 뉴스 조회
-    const cluster = await collection.findOne({ _id: new ObjectId(id) });
+    const cluster = await collection.findOne({ _id: new ObjectId(clusterId) });
     
     if (!cluster) {
       return respond(404, formatErrorResponse('News not found', 'ERROR404'));
